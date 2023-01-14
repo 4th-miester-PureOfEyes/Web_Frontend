c05f6a8e7a8dcd68a7cb76fb02da6cacfe493641
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-01-15 03:17:25 +0900

    add : 검색 부분 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -10,6 +10,7 @@ export const Header = () => {
         <_.HeaderContainer>
             <_.HeaderIn>
                 <_.HeaderInTop>
+                    <_.SearchWrap></_.SearchWrap>
                 </_.HeaderInTop>
                 <_.NavContainer>
                         <_.NavMenu>
