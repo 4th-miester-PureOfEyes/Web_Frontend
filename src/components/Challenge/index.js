a7a377973cde8406e5ae2eb5b0e141543649c3e3
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-01-15 05:54:52 +0900

    add:board

--- src/components/Challenge/index.js
+++ src/components/Challenge/index.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const ChallengeWrap = styled.div`
-background-color: aliceblue;
 height:700px;
 padding-left: 20%;
 padding-right: 20%;
@@ -9,7 +8,7 @@ padding-top: 5%;
 `
 export const Title = styled.div`
 width:100%;
-font-size:1.2em;
+font-size:1.4em;
 font-weight: 600;
 margin-bottom: 10px;
 `
@@ -17,4 +16,8 @@ export const Subtitle = styled.div`
 font-size:1em;
 font-weight: 300;
 color:#898D90;
+`
+export const CardComponent = styled.div`
+display: flex;
+flex-direction:row;
 `
